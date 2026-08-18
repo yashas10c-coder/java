b21535380ca4d1c90a31f62a894e3428ee90f5a2
Yashas V <yashas10c@gmail.com>
2026-08-18 11:03:55 +0530

    Add files via upload

---
+++ overrid.java
@@ -0,0 +1,17 @@
+package train;
+ class hin{
+	void g() {
+		System.out.println("afgj");
+	}
+}
+public class overrid extends hin {
+	void g(){
+		System.out.println("maj");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		overrid j=new overrid();
+		j.g();
+	}
+
+}
